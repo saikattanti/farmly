9894a5dc4fcde20c5406a6055438831349f906c9
LAZYCODER <dipghosal2002@gmail.com>
2024-09-12 16:55:19 +0530

    changes done

--- frontend/src/more/CommingSoon.jsx
+++ frontend/src/more/CommingSoon.jsx
@@ -20,9 +20,9 @@ const CommingSoon = () => {
             <div className='bg'>
                 <span dataText="Comming" className='first'>Comming<span dataText="Soon....">Soon....</span></span>
                 <div className="one">
-                    <div className="circle">
+                    {/* <div className="circle">
 
-                    </div>
+                    </div> */}
                 </div>
             </div>
             </div>
